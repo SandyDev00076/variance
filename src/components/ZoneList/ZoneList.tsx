eda381513543a5dc354d0964bc9470f8fffffb69
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-02-06 16:32:20 +0530

    add sass

---
+++ src/components/ZoneList/ZoneList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './ZoneList.module.scss';
+
+console.log(styles);
+
+const ZoneList = () => {
+  return (
+    <section>
+      <h1>ZoneList</h1>
+    </section>
+  );
+};
+
+export default ZoneList;
