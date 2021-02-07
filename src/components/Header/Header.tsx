91083cd3c8d313ee2b885d75ff700c7066cbae16
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-02-07 15:12:19 +0530

    add timezone list

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import TimeZoneIcon from '../../assets/icons/watch_later-24px.svg';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <header className={styles.container}>
-      <div className={styles.appLine}>
-        <TimeZoneIcon className={styles.timeZoneIcon} />
-        <h1 className={styles.appName}>Variance</h1>
-      </div>
+      <h1 className={styles.appName}>Variance</h1>
     </header>
   );
 };
--- src/components/ZoneList/ZoneList.tsx
+++ src/components/ZoneList/ZoneList.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(timezone);
 
 const ZoneList = () => {
-  return <section>ZoneList</section>;
+  useEffect(() => {
+    console.log(dayjs.tz.guess());
+    console.log(new Date().getTimezoneOffset());
+  }, []);
+  return <section></section>;
 };
 
 export default ZoneList;
