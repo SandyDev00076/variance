7713d7f1a3f4d43ff58c4f3aee4b5a50e8adcc6d
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-02-07 16:02:16 +0530

    add colors

---
+++ src/components/TZSelector/TZSelector.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './TZSelector.module.scss';
+
+const TZSelector = () => {
+  return <div></div>;
+};
+
+export default TZSelector;
--- src/components/ZoneList/ZoneList.tsx
+++ src/components/ZoneList/ZoneList.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
+import TZSelector from '../TZSelector';
 
 import styles from './ZoneList.module.scss';
 
 dayjs.extend(timezone);
 
 const ZoneList = () => {
-  return <section className={styles.container}>ZoneList</section>;
+  return (
+    <section className={styles.container}>
+      <TZSelector />
+    </section>
+  );
 };
 
 export default ZoneList;
